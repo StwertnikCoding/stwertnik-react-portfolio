ef1e22cc94ead2ce50e4eb3e01d645b85145313c
stwertnikcoding <chransondub@gmail.com>
2021-07-24 12:36:12 -0600

    Built Secure Portfolio Manager Class Componenet

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,8 +8,8 @@ const NavigationContainer = (props) => {
   const dyanmicLink = (route, linkText) => {
     return (
       <div className="nav-link-wrapper">
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
+        <NavLink to={route} activeClassName="nav-link-active">
+          {linkText}
         </NavLink>
       </div>
     );
@@ -51,7 +51,14 @@ const NavigationContainer = (props) => {
           </NavLink>
         </div>    
 
-        {props.loggedInStatus === "LOGGED_IN" ? dyanmicLink("/blog", "Blog") : null}
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+        </div>
+
+        {props.loggedInStatus === "LOGGED_IN" ? 
+        dyanmicLink("/portfolio-manager", "Portfolio Manager") : null}
         
       </div>
 

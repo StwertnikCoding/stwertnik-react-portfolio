4ccc4f6a5e7ff09ca7349d8ca85155d824a8c9ce
stwertnikcoding <chransondub@gmail.com>
2021-07-24 17:11:29 -0600

    Built Javascript FormData Object

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -19,6 +19,20 @@ export default class PortfolioForm extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+buildForm () {
+    let formData = new FormData();
+
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.postion);
+
+
+    return formData;
+}
+
 handleChange(event) {
     this.setState({
         [event.target.name]: event.target.value
@@ -26,8 +40,9 @@ handleChange(event) {
 }
 
 handleSubmit(event) {
-    console.log("event", event);
+    this.buildForm();
     event.preventDefault();
+
 }
 
   render() {

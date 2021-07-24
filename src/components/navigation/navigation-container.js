bab87c7e810c53a17b2063065b87071a7cdb780a
stwertnikcoding <chransondub@gmail.com>
2021-07-24 11:24:56 -0600

    Changed NavigationContainer to a functional component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,14 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class NavigationContainer extends Component {
-  constructor() {
-      super();
-  }
+const NavigationContainer = (props) => {
 
-
-
-  render() {
     return (
       <div className="nav-wrapper">
       <div className="left-side">
@@ -45,4 +39,5 @@ export default class NavigationContainer extends Component {
       </div>
     );
   }
-}
+
+  export default NavigationContainer;

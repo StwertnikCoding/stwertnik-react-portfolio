5af563c6916651c734a090ef61a4134476809445
stwertnikcoding <chransondub@gmail.com>
2021-07-31 13:59:50 -0600

    Built initial Blog Detail Component &Set the blog titles as links to said Component

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class BlogDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+            Blog Detail Component
+        </h1>
+      </div>
+    );
+  }
+}

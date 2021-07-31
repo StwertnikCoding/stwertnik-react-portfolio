5293426b4a6d6ea3c5e9c80ce6925c31e4bdf5ae
stwertnikcoding <chransondub@gmail.com>
2021-07-31 13:14:39 -0600

    Converted Blog Component to Class Component from Functional

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function() {
-    return (
-        <div>
-           <h2>
-               Blog
-           </h2>
-           <div>
-               <Link to="/about-me">Read All About Me</Link>
-           </div>
-        
-        </div>
-    );
+export default class Blog extends Component {
+    constructor () {
+        super ();
+    }
+    render () {
+        return (
+            <div>
+            <h2>
+                Blog
+            </h2>
+            <div>
+                <Link to="/about-me">Read All About Me</Link>
+            </div>
+            
+            </div>
+        );
+    }
 }

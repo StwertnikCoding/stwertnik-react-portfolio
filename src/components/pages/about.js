13901e435743fc809e25f7f651e7ed89017fd2d5
stwertnikcoding <chransondub@gmail.com>
2021-08-19 18:16:17 -0600

    Added initial structure to about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function() {
+export default class AboutPage extends Component {
+  render() {
     return (
-        <div>
-            About
-        </div>
-    )
+      <div className="about-page-wrapper">
+          <div className="right-column-wrapper">
+            <div className="about-image-wrapper">
+                <img className="about-image" src={require('../../logo/about-image.png')}></img>
+            </div>
+          </div>
+          <div className="left-column-wrapper">
+            <div className="bio-wrapper">
+                <p>
+                    Here is some sample text of my bio right here
+                </p>
+            </div>
+          </div>
+      </div>
+    );
+  }
 }

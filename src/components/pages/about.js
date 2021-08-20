37e7024a1b730f455efb3e748388c7097c41aaf7
stwertnikcoding <chransondub@gmail.com>
2021-08-19 18:50:37 -0600

    updated a few more things

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -16,8 +16,9 @@ export default class AboutPage extends Component {
                     This is my react portfolio, which I built with the help of my college to showcase
                     and display any and all React applications I build. I also implemented a manager,
                     authorization, and a blog element.
-
-                    I am an 18 year old Full Stack Web Developer! My main language is javascript, and I 
+                </p>
+                <p>
+                        I am an 18 year old Full Stack Web Developer! My main language is javascript, and I 
                     am really liking React Js right now. I also know C++, Java, HTML, Scss, and some Python 
                     I will have my Full Stack Web Development Certificate from Bottega Tech soon here, and 
                     I'm excited to finally get started building some more high level programs.

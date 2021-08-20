7ae695daef5c336add0aea1a20a2c29aad676547
stwertnikcoding <chransondub@gmail.com>
2021-08-20 12:10:04 -0600

    Added basic structure to contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function() {
+export default class ContactPage extends Component {
+  render() {
     return (
-        <div>
-            Contact
+      <div className="contact-page-wrapper">
+        
+        <div className="left-column-wrapper">
+            <div className="contact-image-wrapper">
+                <img className="contact-image" src={require('../../logo/contact-image.png')}></img>
+            </div>
         </div>
+        
+        <div className="right-column-wrapper">
+            <div className="contact-info-wrapper">
+                <div className="contact-icon-wrapper">
+
+                </div>
+
+                <div className="contact-email-wrapper">
+                    stwertnikdevelopment@gmail.com
+                </div>
+            </div>
+
+            <div className="contact-info-wrapper">
+                <div className="contact-icon-wrapper">
+
+                </div>
+
+                <div className="contact-email-wrapper">
+                    chransondub@gmail.com
+                </div>
+            </div>
+        </div>
+      </div>
     );
+  }
 }
